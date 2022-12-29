bcf9f1b07480422dd9183c7d02539a9f5e15a7b2
Mazanov Aleksei <soumirel.rofl@gmail.com>
2022-12-29 21:09:33 +0300

    bugfix in shopPreview JS (wrong link)

--- js/shopPreview.js
+++ js/shopPreview.js
@@ -1,11 +1,11 @@
 previewCards = document.querySelectorAll('.shop-preview-gallery-box');
 
 previewCards[0].addEventListener('click', () => {
-    window.location.href = "corei9_13900k.html";
+    window.location.href = "corei9_13900K.html";
 });
 
 previewCards[1].addEventListener('click', () => {
-    window.location.href = 'r9_7950x.html';
+    window.location.href = 'r9_7950X.html';
 });
 
 previewCards[2].addEventListener('click', () => {
